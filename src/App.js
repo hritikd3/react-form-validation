ab9e86ea976fa7249ed2315cbe6a1650ab8d33c6
hritik <86347286+hritikd3@users.noreply.github.com>
2022-04-10 17:04:42 +0530

    heading updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
-import Form from './Form.js';
+import React, { Component } from "react";
+import "./App.css";
+import Form from "./Form.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          
-          <h2>React Form Task</h2>
+          <h2>React Form </h2>
         </div>
         <Form />
-        
       </div>
     );
   }
